aeb5579e0bdf2aa34eac0fea4f039fe104bd5390
sthe283156w <sthe283156w@icloud.com>
2016-12-12 16:05:40 +0800

    Update hw01.java

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,20 @@
 package hw;
+import java.util.*;
 /*
  * Date: 2016/12/12
  * Author: 105021***  ???
  */
 public class hw01 {
 	public static void main(String[] args) {
-
+		Scanner scn = new Scanner(System.in);
+		String n = scn.nextLine();
+		int p=scn.nextInt();
+			char[]s=n.toCharArray();
+		for(int i=0;i<s.length;i++)
+		{char a=s[i]; int b=a ;
+			System.out.print((char)(b+p));
+		
+		
+		}
 	}
 }
